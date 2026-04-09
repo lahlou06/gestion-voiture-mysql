bae5c3176cb4dbaf498e4e14b26e5280a2c5dddc
lahlou <lachi.lahlou2023@gmail.com>
2026-04-09 15:44:31 -0400

    création de class voiture

---
+++ voiture.py
@@ -0,0 +1,7 @@
+class Voiture:
+    def _init_(self, marque, modele, annee, prix, id=None):
+        self.id = id
+        self.marque = marque
+        self.modele = modele
+        self.annee = annee
+        self.prix = prix
