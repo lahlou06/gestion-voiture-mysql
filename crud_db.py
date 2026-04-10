a8bde8e124508456eccf2691526eca0f1d59f580
lahlou <lachi.lahlou2023@gmail.com>
2026-04-09 22:50:04 -0400

    Ajout de la fonction ajouter_voiture

--- crud_db.py
+++ crud_db.py
@@ -13,3 +13,28 @@ def connecter_db():
     )
 
     return connexion
+def ajouter_voiture(voiture):
+    connexion = connecter_db()
+    curseur = connexion.cursor()
+
+    curseur.execute('\n'
+                    '        CREATE TABLE IF NOT EXISTS voiture (\n'
+                    '            id INT AUTO_INCREMENT PRIMARY KEY,\n'
+                    '            marque VARCHAR(50),\n'
+                    '            modele VARCHAR(50),\n'
+                    '            annee INT,\n'
+                    '            prix DECIMAL(10,2)\n'
+                    '        )\n'
+                    '    ')
+
+    requete = """
+        INSERT INTO voiture (marque, modele, annee, prix)
+        VALUES (%s, %s, %s, %s)
+    """
+    valeurs = (voiture.marque, voiture.modele, voiture.annee, voiture.prix)
+
+    curseur.execute(requete, valeurs)
+    connexion.commit()
+
+    curseur.close()
+    connexion.close()
