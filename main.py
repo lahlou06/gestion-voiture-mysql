dfafc45941420d54d8e971b2976cb0c0a9ba9b94
lahlou <lachi.lahlou2023@gmail.com>
2026-04-09 22:40:22 -0400

    Test de la fonction connecter_db dans main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from crud_db import connecter_db
 
+try:
+    connexion = connecter_db()
+    print("Connexion à la base de données réussie")
+    connexion.close()
+    print("Connexion fermée")
+except Exception as e:
+    print("Erreur de connexion :", e)
