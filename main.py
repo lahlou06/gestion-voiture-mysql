8cc9d9a9604228ad67df2fad8a9503bccf87ca61
lahlou <lachi.lahlou2023@gmail.com>
2026-04-10 00:02:14 -0400

    Projet finale

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ try:
     print("Connexion fermée")
 except Exception as e:
     print("Erreur de connexion :", e)
-v1 =  Voiture("Honda", "Civic", 2019, 17200)
-v2 =Voiture("Toyota", "Prius", 2020, 18500)
+v1 =Voiture("Toyota", "Prius", 2020, 18500)
+v2 =  Voiture("Honda", "Civic", 2019, 17200)
 v3 = Voiture("Audi", "A3", 2018, 24500)
 
 ajouter_voiture(v1)
@@ -19,11 +19,12 @@ voitures = recuperer_voitures()
 print("Voitures après ajout :")
 for v in voitures:
     v.afficher_voiture()
-supprimer_voiture(1)
+supprimer_voiture(0)
 print("Voiture supprimée avec succès")
 
 print("Voitures après suppression :")
-v.afficher_voiture()
+for v in voitures:
+    v.afficher_voiture()
 voitures = recuperer_voitures()
 print("Voitures après récupération :")
 for v in voitures:
